9bfdec1aadbb9b0ff534d3e6b442ab9e886018fa
Ashwin Maharjan <maharjan.ashwin098@gmail.com>
2026-05-11 21:46:31 +0545

    day 1 of learning laravel

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,5 @@
 <h1>Homepage</h1>
+
+<a href="/">Click here for HomePage</a> <br>
+<a href="/about/ashwin">Click here for About of Ashwin</a> <br>
+<a href="/welcome">Click here for WelcomePage</a>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,25 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/welcome', function () {
+    return view('welcome');
+});
+
+Route::get('/home', function () {
+    return view('home');
+});
+
 Route::get('/', function () {
     return view('home');
 });
+
+Route::view('/ashwin','ashwin');
+
+
+Route::get('/about/{name}',function($name){
+    return view('about',['name'=>$name]);
+});
+
+Route::redirect('/home','/welcome'); //redirect with routing
+
+
